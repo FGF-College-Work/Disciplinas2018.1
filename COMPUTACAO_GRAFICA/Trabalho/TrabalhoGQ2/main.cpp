3844ec748a4d6b8c7e01eb4b055e7d5b74bfda7a
marcialwushu <marcialwushu@gmail.com>
2018-04-22 03:20:38 -0300

    geometric figure with openGL and keyboard function add

---
+++ COMPUTACAO_GRAFICA/Trabalho/TrabalhoGQ2/main.cpp
@@ -0,0 +1,94 @@
+#include <windows.h>
+#include <gl/glut.h>
+#include <stdlib.h>
+
+GLfloat escala = 1;
+
+void Desenha(){
+
+
+
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
+    //Limpa janela de visualização
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    // Especifica que a coor  corrente é vermelha
+    // RGB
+
+    glScalef(escala,escala,0);
+
+
+    glBegin(GL_QUADS);
+                glVertex2i(100,150);
+                glVertex2i(100,100);
+                // Especifica que a cor corrente é azul
+                glColor3f(0.0f, 0.0f, 1.0f);
+                glVertex2i(150,100);
+                glVertex2i(150,150);
+    glEnd();
+
+    // Executa o comandos openGL
+    glFlush();
+
+}
+
+void Inicializa(void){
+
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+}
+
+void AlteraTamanhoJanela(GLsizei w, GLsizei h){
+
+    if(h == 0) h = 1;
+
+
+
+    glViewport(0, 0, w, h);
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+
+
+    if (w <= h)
+        gluOrtho2D(0.0f, 250.0f, 0.0f, 250.0f*h/w);
+    else
+        gluOrtho2D(0.0f, 250.0f*w/h, 0.0f, 250.0f);
+
+
+}
+
+
+// Teclas de zoom
+
+
+void Teclas(unsigned char tecla, GLint x, GLint y){
+    switch(tecla) {
+        case '+' : escala++;
+                    break;
+        case '-' : escala--;
+                    break;
+    }
+    Desenha();
+
+}
+
+
+
+int main(void){
+
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(400,350);
+    glutInitWindowPosition(10,10);
+    glutCreateWindow("Quadrado");
+
+    glutKeyboardFunc(Teclas);
+
+    glutDisplayFunc(Desenha);
+    glutReshapeFunc(AlteraTamanhoJanela);
+    Inicializa();
+    glutMainLoop();
+
+
+}
